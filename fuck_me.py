96ce229f68a0ccd58706e90320576ed68ccc7ed1
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2025-11-08 10:43:47 -0800

    This is not a rieman sum I think

--- fuck_me.py
+++ fuck_me.py
@@ -2,14 +2,14 @@ import math
 
 def calcError(v, phoenemArray, r, t):
     err = 0
-    for i in range(len(phoenemArray)):
-        err += (r((v[i + t]) - phoenemArray[i])**2)
+    n = len(phoenemArray)
+    for i in range(n):
+        err += (r * v[i + t] - phoenemArray[i]) ** 2
     return err
+    
 
-phoenemArray = [1,2,3]
-v = [1,2,3]
-calcError(v, phoenemArray, 3, 4)    
-print(4)
+p_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+v_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+why = calcError(v_list, p_list, 1, 0)    
+print(why)
     
-    
-        
