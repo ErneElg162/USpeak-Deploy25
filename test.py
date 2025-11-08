50094daea68e426699b1149351247bc6828b52fd
Ernesto Elguezabal <ernestoelg50@gmail.com>
2025-11-08 10:22:16 -0800

    DAD

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
 from pydub import AudioSegment
-
-def getAmp(audio, index):
-    return int.from_bytes(bleh.get_frame(index), byteorder="little", signed=True)
+from pydub import effects
 
 
-bleh = AudioSegment.from_wav("test.wav")
+def getAmp(audio: AudioSegment, index: int) -> int:
+    return int.from_bytes(audio.get_frame(index), byteorder="little", signed=True)
 
-for i in range(100000, 110000):
-    print(getAmp(bleh, i))
+
+
+d = AudioSegment.from_wav("Phonemes/2.wav")
+a = AudioSegment.from_wav("Phonemes/25.wav")
+
+dad = d + a * 3 + d * 3
+
+dad.export("DAD.wav")
+
+
+"""
+numPhonemes = 4
+
+for i in range(1, numPhonemes + 1):
+    a = AudioSegment.from_wav(f"Phonemes/{i}.wav") * 1000
+    a.export(f"TestWav/{i}.wav")
+"""
